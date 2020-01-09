6f586788466c466ae76c0141b7281bf38acb0abd
Donald Sharp <sharpd@cumulusnetworks.com>
2020-01-09 09:01:10 -0500

    zebra: Convert zserv_nexthop_num_warn to return bool
    Allow us to key of the warning if we have one.
    
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>

--- zebra/zapi_msg.c
+++ zebra/zapi_msg.c
@@ -1390,8 +1390,7 @@ stream_failure:
 	return;
 }
 
-
-void zserv_nexthop_num_warn(const char *caller, const struct prefix *p,
+bool zserv_nexthop_num_warn(const char *caller, const struct prefix *p,
 			    const unsigned int nexthop_num)
 {
 	if (nexthop_num > zrouter.multipath_num) {
@@ -1402,7 +1401,10 @@ void zserv_nexthop_num_warn(const char *caller, const struct prefix *p,
 			EC_ZEBRA_MORE_NH_THAN_MULTIPATH,
 			"%s: Prefix %s has %d nexthops, but we can only use the first %d",
 			caller, buff, nexthop_num, zrouter.multipath_num);
+		return true;
 	}
+
+	return false;
 }
 
 static void zread_route_add(ZAPI_HANDLER_ARGS)
