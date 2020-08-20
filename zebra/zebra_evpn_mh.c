28756236f310b6a8e140d8aaa9c48b6114bec9a5
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-08-20 16:22:06 -0700

    zebra: enable arp-reply/NA snooping on br members for fast-failover in MH
    If multihoming is enabled with fast-failover we need need to redirect
    unicast ARP-replies and Neighbor-Advertisements in user space if the
    DMAC points to an operationally down ES.
    
    If fast failover is enabled we expect the dataplane (ASIC) to redirect
    traffic headed to the down-ES via the VxLAN overlay. However ARP packets
    on bridge access members are not forwarded by the ASIC. They are sent to
    the CPU to be forwarded by the kernel post ARP suppression decisions.
    Currently the kernel does not handle fast-failover (traffic redirection).
    So we need to workaround that limitation in user space.
    
    We do the user-space redirection currently in zebra as it has all the
    necessary databases (VLAN->VNI mapping, MAC/FDB and ES tables). A raw
    socket is created for every bridge-access port if MH is enabled (on
    addition of first local ES). This socket listens on ARP-replies and
    NAs. A read thread is also created per-bridge-access-port to read and
    redirect these packets via the VxLAN overlay (vxlan encapsulates and
    sends) if needed.
    
    Use case example:
    1. H11 and H12 are multi-homed to L11 and L12.
    2. Say link between L12<=>H12 is down.
    3. H12 sends a ARP request to H11.
    4. H11 replies via the link attached to L12.
    5. L12 rxes the packet and finds the DMAC (H12) via an ES that is
    oper-down.
    6. L12 (zebra) redirects the packet via (the VxLAN overlay) L11 to H12.
    
    Sample output:
    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
    torm-11# sh evpn arp-nd-redirect
    EVPN ARP-reply/NA redirect: enabled
    Stats:
      IPv4 ARP replies: 11
      IPv6 neighbor advertisements: 538
      Redirected packets: 9
      Skipped packets:    Not ready: 0
        VNI missing: 0
        MAC missing: 25
        Dest is not local ES: 513
        Dest ES oper-up: 2
    torm-11#
    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
    
    Ticket: CM-30135
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- zebra/zebra_evpn_mh.c
+++ zebra/zebra_evpn_mh.c
@@ -49,6 +49,7 @@
 #include "zebra/zebra_vxlan_private.h"
 #include "zebra/zebra_router.h"
 #include "zebra/zebra_evpn_mh.h"
+#include "zebra/zebra_evpn_arp_nd.h"
 #include "zebra/zebra_nhg.h"
 
 DEFINE_MTYPE_STATIC(ZEBRA, ZACC_BD, "Access Broadcast Domain");
@@ -539,7 +540,7 @@ static bool zebra_evpn_acc_vl_cmp(const void *p1, const void *p2)
 }
 
 /* Lookup VLAN based broadcast domain */
-static struct zebra_evpn_access_bd *zebra_evpn_acc_vl_find(vlanid_t vid)
+struct zebra_evpn_access_bd *zebra_evpn_acc_vl_find(vlanid_t vid)
 {
 	struct zebra_evpn_access_bd *acc_bd;
 	struct zebra_evpn_access_bd tmp;
@@ -2134,6 +2135,17 @@ static void zebra_evpn_mh_on_first_local_es(void)
 	zebra_evpn_mh_advertise_svi_mac();
 }
 
+/* currently there is no global config to turn on MH instead we use
+ * the addition of the first local Ethernet Segment as the trigger to
+ * init MH specific processing
+ */
+static void zebra_evpn_mh_on_first_local_es(void)
+{
+	zebra_evpn_mh_dup_addr_detect_off();
+	zebra_evpn_mh_advertise_reach_neigh_only();
+	zebra_evpn_arp_nd_failover_enable();
+}
+
 static void zebra_evpn_es_local_info_set(struct zebra_evpn_es *es,
 		struct zebra_if *zif)
 {
@@ -3534,6 +3546,8 @@ void zebra_evpn_es_set_base_evpn(struct zebra_evpn *zevpn)
 			zebra_evpn_es_re_eval_send_to_client(es,
 					true /* es_evi_re_reval */);
 	}
+
+	zebra_evpn_arp_nd_udp_sock_create();
 }
 
 /* called when a vni is removed or becomes oper down or is removed from a
