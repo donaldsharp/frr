d28f42b7bd90bf95d27342851ca3943d7c64edf8
Stephen Worley <sworley@nvidia.com>
2021-04-28 15:45:29 -0400

    zebra: handle STP state change for SVD per vlan ID
    Read in STP state changes for a Single Vxlan Device
    via bridge vlan netlink messages. Map the vlanid to a
    VNI in the SVD table and treat it similar to how
    we handle proto down of the Vxlan device traditionally
    in a non-SVD device scenario.
    
    Forwarding == Interface UP
    Blocking == Interface DOWN
    
    Signed-off-by: Stephen Worley <sworley@nvidia.com>

--- zebra/rtread_netlink.c
+++ zebra/rtread_netlink.c
@@ -27,6 +27,7 @@
 #include "zebra/rt.h"
 #include "zebra/zebra_pbr.h"
 #include "zebra/rt_netlink.h"
+#include "zebra/if_netlink.h"
 #include "zebra/rule_netlink.h"
 
 void route_read(struct zebra_ns *zns)
@@ -75,6 +76,11 @@ void neigh_read_specific_ip(const struct ipaddr *ip, struct interface *vlan_if)
 void kernel_read_pbr_rules(struct zebra_ns *zns)
 {
 	netlink_rules_read(zns);
+}
+
+void vlan_read(struct zebra_ns *zns)
+{
+	netlink_vlan_read(zns);
 }
 
 #endif /* GNU_LINUX */
