85d34759f392de47fffa6b16baca96b15809f8a6
Donatas Abraitis <donatas.abraitis@gmail.com>
2021-07-02 23:48:16 +0300

    bgpd: Show max packet size per update-group
    ```
    exit1-debian-9# sh ip bgp update-groups
    Update-group 2:
      Created: Fri Jul  2 20:47:23 2021
      MRAI value (seconds): 0
    
      Update-subgroup 2:
        Created: Fri Jul  2 20:47:23 2021
        Join events: 1
        Prune events: 0
        Merge events: 0
        Split events: 0
        Update group switch events: 0
        Peer refreshes combined: 0
        Merge checks triggered: 0
        Coalesce Time: 1200
        Version: 14
        Packet queue length: 0
        Total packets enqueued: 8
        Packet queue high watermark: 1
        Adj-out list count: 11
        Advertise list: empty
        Flags:
        Max packet size: 65535
        Peers:
          - eth1
    Update-group 3:
      Created: Fri Jul  2 20:47:43 2021
      MRAI value (seconds): 0
    
      Update-subgroup 3:
        Created: Fri Jul  2 20:47:43 2021
        Join events: 1
        Prune events: 0
        Merge events: 0
        Split events: 0
        Update group switch events: 0
        Peer refreshes combined: 0
        Merge checks triggered: 0
        Coalesce Time: 1200
        Version: 14
        Packet queue length: 0
        Total packets enqueued: 8
        Packet queue high watermark: 1
        Adj-out list count: 11
        Advertise list: empty
        Flags:
        Max packet size: 4096
        Peers:
          - 192.168.10.17
    ```
    
    Signed-off-by: Donatas Abraitis <donatas.abraitis@gmail.com>

--- bgpd/bgp_updgrp.c
+++ bgpd/bgp_updgrp.c
@@ -574,6 +574,7 @@ static int update_group_show_walkcb(struct update_group *updgrp, void *arg)
 	struct update_subgroup *subgrp;
 	struct peer_af *paf;
 	struct bgp_filter *filter;
+	struct peer *peer = UPDGRP_PEER(updgrp);
 	int match = 0;
 	json_object *json_updgrp = NULL;
 	json_object *json_subgrps = NULL;
@@ -794,6 +795,8 @@ static int update_group_show_walkcb(struct update_group *updgrp, void *arg)
 					   SUBGRP_FLAG_NEEDS_REFRESH)
 					? "R"
 					: "");
+			if (peer)
+				vty_out(vty, "    Max packet size: %d\n",
 		}
 		if (subgrp->peer_count > 0) {
 			if (ctx->uj) {
