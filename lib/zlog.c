2bacb368666dc19519999061db0d34a1456ab453
Anuradha Karuppiah <anuradhak@nvidia.com>
2021-09-23 16:45:37 -0700

    [CUMULUS] lib: disable conversion of logs into tracepoints
    ospf6d precommit tests were failing with ospf6d crashing when ospf6
    debug logs were enabled and installed as tracepoints.
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@nvidia.com>

--- lib/zlog.c
+++ lib/zlog.c
@@ -451,6 +451,7 @@ void vzlog(int prio, const char *fmt, va_list ap)
 {
 	struct zlog_tls *zlog_tls = zlog_tls_get();
 
+#ifndef HAVE_CUMULUS
 #ifdef HAVE_LTTNG
 	va_list copy;
 	va_copy(copy, ap);
@@ -478,6 +479,7 @@ void vzlog(int prio, const char *fmt, va_list ap)
 	va_end(copy);
 	XFREE(MTYPE_LOG_MESSAGE, msg);
 #endif
+#endif /* HAVE_CUMULUS */
 
 	if (zlog_tls)
 		vzlog_tls(zlog_tls, prio, fmt, ap);
