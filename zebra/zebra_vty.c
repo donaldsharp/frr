0e1faf853fd9b1d5be606e8ade5eb26a34c4af52
Mark Stapp <mstapp@nvidia.com>
2022-06-28 10:18:02 -0400

    zebra: clean up minor clang-format complaints
    Clang-format complains about a couple of 'frr_each' uses;
    clean them up.
    
    Signed-off-by: Mark Stapp <mstapp@nvidia.com>

--- zebra/zebra_vty.c
+++ zebra/zebra_vty.c
@@ -1535,8 +1535,8 @@ static void show_nexthop_group_out(struct vty *vty, struct nhg_hash_entry *nhe)
 
 	if (!zebra_nhg_dependents_is_empty(nhe)) {
 		vty_out(vty, "     Dependents:");
-		frr_each(nhg_connected_tree, &nhe->nhg_dependents,
-			 rb_node_dep) {
+		frr_each (nhg_connected_tree, &nhe->nhg_dependents,
+			  rb_node_dep) {
 			vty_out(vty, " (%u)", rb_node_dep->nhe->id);
 		}
 		vty_out(vty, "\n");
@@ -1614,8 +1614,8 @@ static void if_nexthop_group_dump_vty(struct vty *vty, struct interface *ifp)
 	if (!if_nhg_dependents_is_empty(ifp)) {
 		vty_out(vty, "Interface %s:\n", ifp->name);
 
-		frr_each(nhg_connected_tree, &zebra_if->nhg_dependents,
-			 rb_node_dep) {
+		frr_each (nhg_connected_tree, &zebra_if->nhg_dependents,
+			  rb_node_dep) {
 			vty_out(vty, "   ");
 			show_nexthop_group_out(vty, rb_node_dep->nhe);
 		}
