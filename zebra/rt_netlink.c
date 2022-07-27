6ad1200b4ac9a02da2596b2419a8b3297988f784
Chirag Shah <chirag@nvidia.com>
2022-07-26 20:49:49 -0700

    zebra: add tracepoint in kernel route update
    add TP in kernel route update with NHG-Id
    
    Ticket:#3094249
    
    Signed-off-by: Chirag Shah <chirag@nvidia.com>

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -2233,11 +2233,16 @@ ssize_t netlink_route_multipath_msg_encode(int cmd,
 	     && /* CUMULUS ONLY */ !nexthop_group_has_label(
 		     dplane_ctx_get_ng(ctx)))
 	    || (fpm && force_nhg)) {
+		char buf[2];
+		uint32_t nhg_id;
+
+		buf[0] = '\0';
+		nhg_id = dplane_ctx_get_nhe_id(ctx);
 
 		/* Kernel supports nexthop objects */
 		if (IS_ZEBRA_DEBUG_KERNEL)
 			zlog_debug("%s: %pFX nhg_id is %u", __func__, p,
-				   dplane_ctx_get_nhe_id(ctx));
+				   nhg_id);
 
 		if (!nl_attr_put32(&req->n, datalen, RTA_NH_ID,
 				   dplane_ctx_get_nhe_id(ctx)))
@@ -2262,6 +2267,8 @@ ssize_t netlink_route_multipath_msg_encode(int cmd,
 					return 0;
 			}
 		}
+		frrtrace(4, frr_zebra, netlink_route_multipath_msg_encode, p,
+			 cmd, nhg_id, buf, datalen);
 
 		return NLMSG_ALIGN(req->n.nlmsg_len);
 	}
@@ -2426,7 +2433,7 @@ ssize_t netlink_route_multipath_msg_encode(int cmd,
 			strlcat(buf, buf1, sizeof(buf));
 		}
 		frrtrace(4, frr_zebra, netlink_route_multipath_msg_encode, p,
-			 cmd, datalen, buf);
+			 cmd, 0, buf, datalen);
 	}
 
 	return NLMSG_ALIGN(req->n.nlmsg_len);
--- zebra/zebra_trace.h
+++ zebra/zebra_trace.h
@@ -308,14 +308,17 @@ TRACEPOINT_EVENT(
 	TP_ARGS(
 		const struct prefix *, p,
 		int, cmd,
-		size_t, datalen,
-		char *, nexthop),
+		uint32_t, nhg_id,
+		char *, nexthop,
+		size_t , datalen),
 	TP_FIELDS(
 		ctf_string(family, (p->family == AF_INET) ? "AF_INET" : "AF_INET6")
-		ctf_integer(unsigned int, dst_len, p->prefixlen)
+		ctf_array(unsigned char, pfx, p, sizeof(struct prefix))
+		ctf_integer(unsigned int, pfxlen, p->prefixlen)
 		ctf_integer(uint8_t, cmd, cmd)
-		ctf_integer(uint32_t, datalen, datalen)
+		ctf_integer(unsigned int, nhg_id, nhg_id)
 		ctf_string(nexthops, nexthop)
+		ctf_integer(uint32_t, datalen, datalen)
 		)
 	)
 
@@ -325,13 +328,11 @@ TRACEPOINT_EVENT(
 	frr_zebra,
 	netlink_nexthop_msg_encode,
 	TP_ARGS(
-		const struct nexthop *, nh,
-		uint32_t, id,
-		char *, label_buf),
+		const struct nexthop *, nh, uint32_t, nhg_id, char *, label_buf),
 	TP_FIELDS(
 		ctf_integer(uint32_t, nh_index, nh->ifindex)
 		ctf_integer(uint32_t, nh_vrfid, nh->vrf_id)
-		ctf_integer(uint32_t, id, id)
+		ctf_integer(uint32_t, nhg_id, nhg_id)
 		ctf_string(label_buf, label_buf)
 		)
 	)
