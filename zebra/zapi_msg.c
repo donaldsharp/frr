020595d8d1d72d6aea295ed4f4ec45ad1ec2d1bd
Mark Stapp <mstapp@nvidia.com>
2022-10-26 11:12:16 -0700

    zebra: use MAX stream for mrib query zapi reply
    Use a larger stream buffer for the pim mrib query reply -
    we were using a small buffer, and it was too small, leading to
    asserts in the stream lib module.
    
    Signed-off-by: Mark Stapp <mstapp@nvidia.com>

--- zebra/zapi_msg.c
+++ zebra/zapi_msg.c
@@ -695,7 +695,7 @@ static int zsend_nexthop_lookup_mrib(struct zserv *client, struct ipaddr *addr,
 	struct nexthop *nexthop;
 
 	/* Get output stream. */
-	s = stream_new(ZEBRA_MIN_PACKET_SIZE);
+	s = stream_new(ZEBRA_MAX_PACKET_SIZ);
 	stream_reset(s);
 
 	/* Fill in result. */
