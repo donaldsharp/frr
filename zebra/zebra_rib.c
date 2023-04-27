4e0375525f4e995cb7f4851c787fbae12321597c
Donald Sharp <sharpd@nvidia.com>
2023-04-26 23:02:09 -0400

    zebra: Use zebra dplane for RTM link and addr
    a) Move the reads of link and address information
    into the dplane
    b) Move the startup read of data into the dplane
    as well.
    c) Break up startup reading of the linux kernel data
    into multiple phases.  As that we have implied ordering
    of data that must be read first and if the dplane has
    taken over some data reading then we must delay initial
    read-in of other data.
    
    Fixes: #13288
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- zebra/zebra_rib.c
+++ zebra/zebra_rib.c
@@ -5089,7 +5089,9 @@ static void rib_process_dplane_results(struct thread *thread)
 			case DPLANE_OP_NEIGH_TABLE_UPDATE:
 			case DPLANE_OP_GRE_SET:
 			case DPLANE_OP_NONE:
+				break;
 			case DPLANE_OP_STARTUP_STAGE:
+				zebra_ns_startup_continue(ctx);
 				break;
 
 			} /* Dispatch by op code */
