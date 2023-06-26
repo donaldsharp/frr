795ae83ee36b52bf0f0c4733941a947d9e598a54
Chirag Shah <chirag@nvidia.com>
2023-06-26 15:17:07 -0700

    zebra: suppress evpn nhops from route sync path
    Cumulus commit-id ecb2a02c52 suppressed evpn nexthop
    installation along with route sync from bgp to zebra.
    EVPN nexthop (RMAC-VTEP) is sync using MH nexthop
    sync path.
    Post upgrade to 8.4.3 the nexthops are installed
    from two paths so comment out from bgp to zebra
    route install path.
    
    [CUMULUS]: This patch can be upstream along with
    commit-id 038ff7e67b and ecb2a02c52 changes.
    
    Ticket:#3486989
    Reviewed By:
    Testing Done:
    
    Signed-off-by: Chirag Shah <chirag@nvidia.com>

--- zebra/zapi_msg.c
+++ zebra/zapi_msg.c
@@ -1633,8 +1633,10 @@ static struct nexthop *nexthop_from_zapi(const struct zapi_nexthop *api_nh,
 			vtep_ip.ipa_type = IPADDR_V4;
 			memcpy(&(vtep_ip.ipaddr_v4), &(api_nh->gate.ipv4),
 			       sizeof(struct in_addr));
-			zebra_rib_queue_evpn_route_add(
-				api_nh->vrf_id, &api_nh->rmac, &vtep_ip, p);
+			/*
+			 * zebra_rib_queue_evpn_route_add(
+			 * 	api_nh->vrf_id, &api_nh->rmac, &vtep_ip, p);
+			 */
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_EVPN);
 		}
 		break;
@@ -1667,8 +1669,10 @@ static struct nexthop *nexthop_from_zapi(const struct zapi_nexthop *api_nh,
 			vtep_ip.ipa_type = IPADDR_V6;
 			memcpy(&vtep_ip.ipaddr_v6, &(api_nh->gate.ipv6),
 			       sizeof(struct in6_addr));
-			zebra_rib_queue_evpn_route_add(
-				api_nh->vrf_id, &api_nh->rmac, &vtep_ip, p);
+			/*
+			 * zebra_rib_queue_evpn_route_add(
+			 * 	api_nh->vrf_id, &api_nh->rmac, &vtep_ip, p);
+			 */
 			SET_FLAG(nexthop->flags, NEXTHOP_FLAG_EVPN);
 		}
 		break;
