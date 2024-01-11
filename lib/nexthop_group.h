e5bcf76ed5e92cc0cbdd90a79c35d346fb62298b
Rajesh Varatharaj <rvaratharaj@nvidia.com>
2024-01-11 11:53:45 -0800

    lib: Adding nexthop2str purpose is to look up a list of nexthops and convert it into a string.
    Copy the list of nexthops to a string
    
    Ticket: #3357402
    Signed-off-by: Rajesh Varatharaj <rvaratharaj@nvidia.com>

--- lib/nexthop_group.h
+++ lib/nexthop_group.h
@@ -155,6 +155,8 @@ nexthop_group_active_nexthop_num_no_recurse(const struct nexthop_group *nhg);
 
 extern bool nexthop_group_has_label(const struct nexthop_group *nhg);
 
+const char *nexthop_group2str(const struct nexthop_group *nhg, char *str,
+			      size_t size);
 #ifdef __cplusplus
 }
 #endif
