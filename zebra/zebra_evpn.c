db268a6245eb3f69e34085ede4392cc83d815275
Mark Stapp <mjs@cisco.com>
2024-09-20 12:09:05 -0400

    zebra: unlock if_table route_nodes
    Must unlock if we break during iteration over any lib/table
    tree.
    
    Signed-off-by: Mark Stapp <mjs@cisco.com>

--- zebra/zebra_evpn.c
+++ zebra/zebra_evpn.c
@@ -712,6 +712,7 @@ static int zebra_evpn_map_vlan_ns(struct ns *ns,
 				zebra_vxlan_if_access_vlan_vni_find(zif, br_if);
 			if (vni_id) {
 				found = 1;
+				route_unlock_node(rn);
 				break;
 			}
 		}
@@ -808,6 +809,7 @@ static int zebra_evpn_from_svi_ns(struct ns *ns,
 				zebra_vxlan_if_access_vlan_vni_find(zif, br_if);
 			if (vni_id) {
 				found = 1;
+				route_unlock_node(rn);
 				break;
 			}
 		}
@@ -895,6 +897,7 @@ static int zvni_map_to_macvlan_ns(struct ns *ns,
 
 		if (zif->link == in_param->svi_if) {
 			*p_ifp = tmp_if;
+			route_unlock_node(rn);
 			return NS_WALK_STOP;
 		}
 	}
