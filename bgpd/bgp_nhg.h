5992fe7ed5166d3a3391b9cac58ee1aadb8edc76
Karthikeya Venkat Muppalla <kmuppalla@nvidia.com>
2024-09-20 10:33:45 -0700

    bgpd: [SOO] per src nhg id management in bgp
    Enchance the existing bgp nhg api's so that they can be used to
    manage per source nhg for SOO
    
    Ticket: #3951671
    Signed-off-by: Karthikeya Venkat Muppalla <kmuppalla@nvidia.com>

--- bgpd/bgp_nhg.h
+++ bgpd/bgp_nhg.h
@@ -9,9 +9,21 @@
 
 #include "nexthop_group.h"
 
+typedef enum { EVPN_MH = 0, PER_SRC_NHG = 1, APP_MAX } bgp_nhg_app_t;
+
+typedef struct {
+	bitfield_t bitmap;
+	uint32_t start_id;
+	uint32_t id_max;
+} bgp_nhg_app_info_t;
+
+#define EVPN_MH_NH_ID_SPACE (16 * 1024)
+#define PER_SRC_NH_ID_SPACE (16 * 1024)
+#define DEFAULT_ID_SPACE    (16 * 1024)
+
 /* APIs for setting up and allocating L3 nexthop group ids */
-extern uint32_t bgp_nhg_id_alloc(void);
-extern void bgp_nhg_id_free(uint32_t nhg_id);
+extern uint32_t bgp_nhg_id_alloc(bgp_nhg_app_t app);
+extern void bgp_nhg_id_free(bgp_nhg_app_t app, uint32_t nhg_id);
 extern void bgp_nhg_init(void);
 void bgp_nhg_finish(void);
 
