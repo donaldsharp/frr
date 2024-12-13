donaldsharp/frr

commit bdb5ae8bce94432eb5e581f04f48dc4aa5db7ca4

bgpd: Make `suppress-fib-pending` clear peering
When a peer has come up and already started installing
routes into the rib and `suppress-fib-pending` is either
turned on or off.  BGP is left with some routes that
may need to be withdrawn from peers and routes that
it does not know the status of.  Clear the BGP peers
for the interesting parties and let's let us come
up to speed as needed.

Signed-off-by: Donald Sharp <sharpd@nvidia.com>

bgpd/bgpd.c
@@ -410,6 +410,9 @@ void bgp_router_id_static_set(struct bgp *bgp, struct in_addr id)
 void bm_wait_for_fib_set(bool set)
 {
 	bool send_msg = false;
+	struct bgp *bgp;
+	struct peer *peer;
+	struct listnode *next, *node;
 
 	if (bm->wait_for_fib == set)
 		return;
@@ -428,12 +431,32 @@ void bm_wait_for_fib_set(bool set)
 	if (send_msg && zclient)
 		zebra_route_notify_send(ZEBRA_ROUTE_NOTIFY_REQUEST,
 					zclient, set);
+
+	/*
+	 * If this is configed at a time when peers are already set
+	 * FRR needs to reset the connection(s) as that some installs
+	 * may have already happened in some shape fashion or form
+	 * let's just start over
+	 */
+	for (ALL_LIST_ELEMENTS_RO(bm->bgp, next, bgp)) {
+		for (ALL_LIST_ELEMENTS_RO(bgp->peer, node, peer)) {
+			if (!BGP_IS_VALID_STATE_FOR_NOTIF(
+				    peer->connection->status))
+				continue;
+
+			peer->last_reset = PEER_DOWN_SUPPRESS_FIB_PENDING;
+			bgp_notify_send(peer->connection, BGP_NOTIFY_CEASE,
+					BGP_NOTIFY_CEASE_CONFIG_CHANGE);
+		}
+	}
 }
 
 /* Set the suppress fib pending for the bgp configuration */
 void bgp_suppress_fib_pending_set(struct bgp *bgp, bool set)
 {
 	bool send_msg = false;
+	struct peer *peer;
+	struct listnode *node;
 
 	if (bgp->inst_type == BGP_INSTANCE_TYPE_VIEW)
 		return;
@@ -464,6 +487,21 @@ void bgp_suppress_fib_pending_set(struct bgp *bgp, bool set)
 		if (zclient)
 			zebra_route_notify_send(ZEBRA_ROUTE_NOTIFY_REQUEST,
 					zclient, set);
+	}
+
+	/*
+	 * If this is configed at a time when peers are already set
+	 * FRR needs to reset the connection as that some installs
+	 * may have already happened in some shape fashion or form
+	 * let's just start over
+	 */
+	for (ALL_LIST_ELEMENTS_RO(bgp->peer, node, peer)) {
+		if (!BGP_IS_VALID_STATE_FOR_NOTIF(peer->connection->status))
+			continue;
+
+		peer->last_reset = PEER_DOWN_SUPPRESS_FIB_PENDING;
+		bgp_notify_send(peer->connection, BGP_NOTIFY_CEASE,
+				BGP_NOTIFY_CEASE_CONFIG_CHANGE);
 	}
 }
 

bgpd/bgp_fsm.c
@@ -562,42 +562,45 @@ void bgp_delayopen_timer(struct event *thread)
 }
 
 /* BGP Peer Down Cause */
-const char *const peer_down_str[] = {"",
-				     "Router ID changed",
-				     "Remote AS changed",
-				     "Local AS change",
-				     "Cluster ID changed",
-				     "Confederation identifier changed",
-				     "Confederation peer changed",
-				     "RR client config change",
-				     "RS client config change",
-				     "Update source change",
-				     "Address family activated",
-				     "Admin. shutdown",
-				     "User reset",
-				     "BGP Notification received",
-				     "BGP Notification send",
-				     "Peer closed the session",
-				     "Neighbor deleted",
-				     "Peer-group add member",
-				     "Peer-group delete member",
-				     "Capability changed",
-				     "Passive config change",
-				     "Multihop config change",
-				     "NSF peer closed the session",
-				     "Intf peering v6only config change",
-				     "BFD down received",
-				     "Interface down",
-				     "Neighbor address lost",
-				     "No path to specified Neighbor",
-				     "Waiting for Peer IPv6 LLA",
-				     "Waiting for VRF to be initialized",
-				     "No AFI/SAFI activated for peer",
-				     "AS Set config change",
-				     "Waiting for peer OPEN",
-				     "Reached received prefix count",
-				     "Socket Error",
-				     "Admin. shutdown (RTT)"};
+const char *const peer_down_str[] = {
+	"",
+	"Router ID changed",
+	"Remote AS changed",
+	"Local AS change",
+	"Cluster ID changed",
+	"Confederation identifier changed",
+	"Confederation peer changed",
+	"RR client config change",
+	"RS client config change",
+	"Update source change",
+	"Address family activated",
+	"Admin. shutdown",
+	"User reset",
+	"BGP Notification received",
+	"BGP Notification send",
+	"Peer closed the session",
+	"Neighbor deleted",
+	"Peer-group add member",
+	"Peer-group delete member",
+	"Capability changed",
+	"Passive config change",
+	"Multihop config change",
+	"NSF peer closed the session",
+	"Intf peering v6only config change",
+	"BFD down received",
+	"Interface down",
+	"Neighbor address lost",
+	"No path to specified Neighbor",
+	"Waiting for Peer IPv6 LLA",
+	"Waiting for VRF to be initialized",
+	"No AFI/SAFI activated for peer",
+	"AS Set config change",
+	"Waiting for peer OPEN",
+	"Reached received prefix count",
+	"Socket Error",
+	"Admin. shutdown (RTT)",
+	"Suppress Fib Turned On or Off",
+};
 
 static void bgp_graceful_restart_timer_off(struct peer_connection *connection,
 					   struct peer *peer)

bgpd/bgpd.h
@@ -1757,6 +1757,7 @@ struct peer {
 #define PEER_DOWN_PFX_COUNT             33U /* Reached received prefix count */
 #define PEER_DOWN_SOCKET_ERROR          34U /* Some socket error happened */
 #define PEER_DOWN_RTT_SHUTDOWN          35U /* Automatically shutdown due to RTT */
+#define PEER_DOWN_SUPPRESS_FIB_PENDING	 36U /* Suppress fib pending changed */
 	/*
 	 * Remember to update peer_down_str in bgp_fsm.c when you add
 	 * a new value to the last_reset reason

commit 2e31a0282f43894a1c28e322c0526b534ec4dca2

Pull request: https://github.com/FRRouting/frr/pull/17487 bgpd: Do not reset peers on suppress-fib toggling If the desired state is the same - do nothing instead of resetting once again.
Fixes: bdb5ae8 (bgpd: Make suppress-fib-pending clear peering)

Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

bgpd/bgpd.c
@@ -461,6 +461,10 @@ void bgp_suppress_fib_pending_set(struct bgp *bgp, bool set)
 	if (bgp->inst_type == BGP_INSTANCE_TYPE_VIEW)
 		return;
 
+	/* Do nothing if already in a desired state */
+	if (set == !!CHECK_FLAG(bgp->flags, BGP_FLAG_SUPPRESS_FIB_PENDING))
+		return;
+
 	if (set) {
 		SET_FLAG(bgp->flags, BGP_FLAG_SUPPRESS_FIB_PENDING);
 		/* Send msg to zebra for the first instance of bgp enabled