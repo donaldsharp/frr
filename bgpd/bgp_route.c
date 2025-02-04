8fb8bcee9471f0f4ad60788fc9bd91e1254c0959
Donald Sharp <sharpd@nvidia.com>
2025-02-04 10:56:59 -0500

    bgpd: Fix up memory leak in processing eoiu marker
    Memory is being leaked when processing the eoiu marker.
    BGP is creating a dummy dest to contain the data but
    it was never freed.  As well as the eoiu info was
    not being freed either.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -4470,6 +4470,9 @@ static void process_eoiu_marker(struct bgp_dest *dest)
 			   subqueue2str(META_QUEUE_EOIU_MARKER));
 
 	bgp_process_main_one(info->bgp, NULL, 0, 0);
+
+	XFREE(MTYPE_BGP_EOIU_MARKER_INFO, info);
+	XFREE(MTYPE_BGP_NODE, dest);
 }
 
 /*
@@ -4660,6 +4663,7 @@ static void eoiu_marker_queue_free(struct meta_queue *mq, struct bgp_dest_queue 
 		XFREE(MTYPE_BGP_EOIU_MARKER_INFO, dest->info);
 		STAILQ_REMOVE_HEAD(l, pq);
 		STAILQ_NEXT(dest, pq) = NULL; /* complete unlink */
+		XFREE(MTYPE_BGP_NODE, dest);
 		mq->size--;
 	}
 }
